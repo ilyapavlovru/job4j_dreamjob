c1788d39e6c283a1cb1754dc948ac2791d291805
Ilya Pavlov <ilya.pavlov@list.ru>
2021-02-28 17:04:31 +0300

    1. Servlet. Web.xml[6866#345795]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -4,9 +4,9 @@ import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.util.Collection;
-import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Store {
 
@@ -16,10 +16,12 @@ public class Store {
 
     private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
+    private static AtomicInteger postId = new AtomicInteger(4);
+
     private Store() {
-        posts.put(1, new Post(1, "Junior Java Job", "need Junior skills", new Date()));
-        posts.put(2, new Post(2, "Middle Java Job", "need Middle skills", new Date()));
-        posts.put(3, new Post(3, "Senior Java Job", "need Senior skills", new Date()));
+        posts.put(1, new Post(1, "Junior Java Job"));
+        posts.put(2, new Post(2, "Middle Java Job"));
+        posts.put(3, new Post(3, "Senior Java Job"));
         candidates.put(1, new Candidate(1, "Junior Java"));
         candidates.put(2, new Candidate(2, "Middle Java"));
         candidates.put(3, new Candidate(3, "Senior Java"));
@@ -35,5 +37,10 @@ public class Store {
 
     public Collection<Candidate> findAllCandidates() {
         return candidates.values();
+    }
+
+    public void save(Post post) {
+        post.setId(postId.incrementAndGet());
+        posts.put(post.getId(), post);
     }
 }
